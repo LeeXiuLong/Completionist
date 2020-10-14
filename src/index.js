607c66da597bfebabc7bb6a33cb0f15ba29db8c6
Jourdan Ooi <jungterooi@gmail.com>
2020-10-14 12:43:06 -0400

    Finished up the json file maker for the project

--- src/index.js
+++ src/index.js
@@ -4,59 +4,120 @@ const getGameDetail = appId => {
     return new URL(`https://cors-anywhere.herokuapp.com/http://store.steampowered.com/api/appdetails?appids=${appId}`)
 }
 
-let allgames;
+const Chart = require("chart.js");
 
-const getGames = (games) => {
-    return games
-}
+document.addEventListener("DOMContentLoaded", () => {
+    let gameDataUL = document.getElementById("game-list");
 
-const getAllSteamGames = () => {
-    fetch(getAllSteamApps)
-        .then(res => {
-            return res.json()
-        }).then(data => {
-            let gameIds = data.applist.apps.map(datum=>{
-                return datum.appid;
-            }).slice(0,300);
-            let gameNames = data.applist.apps.map(datum => {
-                return datum.name;
-            })
-            const ul = document.getElementById("game-list");
-            const gamePrices = [];
-            gameIds.forEach((id, i) => {
-                fetch(getGameDetail(id)).then(res => {
-                    return res.json()})
-                    .then(data => {
-                        if(data[id].success && data[id].data.type === "game"){
-                            console.log(data[id]);
-                            let newGame = {
-                                id: {
-                                    name: data[id].data.name,
-                                    price: data[id].data.price_overview ? data[id].data.price_overview.final : 0,                        
-                                }
-                            }
-                            let li = document.createElement("li");
-                            let p = document.createElement("p");
-                            let p2 = document.createElement("p")
-                            li.appendChild(p);
-                            li.appendChild(p2);
-                            p.innerHTML = newGame.id.name;
-                            p2.innerHTML = newGame.id.price;
-                            ul.appendChild(li);
-                            gamePrices.push(newGame);
+    const json = require("../gameData.json");
+
+    const gamesData = Object.values(json);
+
+    let gameNames = gamesData.map(gameData => {
+        return gameData.name
+    })
+
+    let gamePrices = gamesData.map(gameData => {
+        return gameData.price_overview.final/100
+    })
+
+    let gameRatings = gamesData.map(gameData => {
+        return (gameData.review_info.total_positive/gameData.review_info.total_reviews * 10).toFixed(2);
+    })
+
+    let gameRatingPerDollar = gamesData.map(gameData => {
+        return {
+            x: (gameData.review_info.total_positive/gameData.review_info.total_reviews * 10).toFixed(2),
+            y: gameData.price_overview.final/100
+        }
+    })
+
+    let gamePics = gamesData.map(gameData => {
+        return gameData.header_image;
+    })
+
+    let myPriceChart = document.getElementsByClassName("my-price-chart");
+    let priceChart = new Chart(myPriceChart, {
+        type: "bar", //bar, horizontal bar ,pie, line, doughnut, radar, polarArea
+        data:{
+            labels: gameNames,
+            datasets: [{
+                label: "Price",
+                data: gamePrices, 
+                backgroundColor: "#00d2e6",
+                borderColor: "#666468",
+                borderWidth: 2,
+                hoverBorderWidth: 4,
+                hoverBorderColor: "black"
+            }],
+        },
+        options:{}
+
+    });
+
+    let myRatingChart = document.getElementsByClassName("my-rating-chart");
+    let ratingChart = new Chart(myRatingChart, {
+        type: "bar",
+        data: {
+            labels: gameNames,
+            datasets: [{
+                label: "Rating",
+                data: gameRatings,
+                backgroundColor: "#00d2e6",
+                borderColor: "#666468",
+                borderWidth: 2,
+                hoverBorderWidth: 4,
+                hoverBorderColor: "black"
+            }]
+        }
+    })
+    
+
+    let myRatingPerDollarChart = document.getElementsByClassName("rating-per-dollar-chart");
+    let scatterChart = new Chart(myRatingPerDollarChart, {
+        type: 'scatter',
+        data: {
+            pics: gamePics,
+            labels: gameNames,
+            datasets: [{
+                data: gameRatingPerDollar,
+                backgroundColor: "#00d2e6",
+            }]
+        },
+        options: {
+            tooltips: {
+                callbacks:{
+                    label: function(tooltipItem, data){
+                        return `${data.labels[tooltipItem.index]} \n Price: $${tooltipItem.value} \n Rating: ${tooltipItem.label}/10`;
+                    }
+                }
+            },
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Rating",
+                    },
+                    position: 'bottom'
+                }],
+                yAxes: [{
+                    ticks: {
+                        reverse: true,
+                        callback: function(value, index, values){
+                            return "$" + value
                         }
-                    })
-            })
-            // gameIds.forEach(gameId => {
-            //     fetch(getGameDetail(gameId)).then(res => {
-            //         return res.json()
-            //     }).then(data => {
-            //         ;
-            //     })
-            // })
-        })
-        .catch(error => console.log(error));
-}
+                    },
+                    scaleLabel:{
+                        display: true,
+                        labelString: "Price in $"
+                    }
+                }]
+            }
+        }
+    });
+})
 
-getAllSteamGames();
+
+
 
